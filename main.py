f579a7e70f7562d94ebdfd06cae91b4789272f99
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2024-08-29 21:09:04 +0200

    Netoyage Main

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
 from ArreraDownloadGUI import*
 
-"""
-adownload = CArreraDownload()
-
-adownload.setDownloadFolder()
-
-adownload.setURL("https://www.youtube.com/watch?v=eVli-tstM5E")
-
-adownload.download()
-"""
-
 a = CArreraDGUI()
 
 a.active()
